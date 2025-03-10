e3e960710cbb0feaa7de42a478e0e1382c2dbdd8
a.rivera <a.riveraa99@gmail.com>
2025-03-10 14:01:15 -0500

    Added all header elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header/Header";
 const App = () => {
   return (
-    <div data-theme="bumblebee">
+    <div data-theme="bumblebee" className="h-screen w-screen">
       <Header />
     </div>
   );
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,10 +2,18 @@ import React from "react";
 
 const Header = () => {
   return (
-    <>
-      <h1>Testing</h1>
-      <button className="btn btn-primary">Default</button>
-    </>
+    <div className="flex h-fit w-screen flex-col content-center justify-center p-2.5 outline-1 outline-black">
+      <h1 className="text-center">Super Sticky Notes</h1>
+      <label className="input">
+        <input type="search" placeholder="Search" />
+      </label>
+      <button className="btn btn-primary">New Note</button>
+      <div className="collapse-arrow bg-base-100 border-base-300 collapse border">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title font-semibold">Filter by...</div>
+        <div className="collapse-content text-sm">Inner filters</div>
+      </div>
+    </div>
   );
 };
 
