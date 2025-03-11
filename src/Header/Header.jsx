bfc3170b93c74c23869b37aefaab1e8d37dc7ad7
a.rivera <a.riveraa99@gmail.com>
2025-03-11 12:34:23 -0500

    Created filter with Menu and New Note with button group

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -7,12 +7,78 @@ const Header = () => {
       <label className="input">
         <input type="search" placeholder="Search" />
       </label>
-      <button className="btn btn-primary">New Note</button>
-      <div className="collapse-arrow bg-base-100 border-base-300 collapse border">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title font-semibold">Filter by...</div>
-        <div className="collapse-content text-sm">Inner filters</div>
+      <div className="dropdown dropdown-bottom dropdown-center flex justify-center">
+        <div tabIndex={0} role="button" className="btn m-1">
+          New Note
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-1 flex w-52 content-center justify-center p-2 shadow-sm"
+        >
+          <li>
+            <a>Item 1</a>
+          </li>
+        </ul>
       </div>
+      {/*TESTER DROPDOWN*/}
+      <div className="dropdown dropdown-bottom dropdown-center flex justify-center">
+        <div tabIndex={0} role="button" className="btn m-1">
+          Click
+        </div>
+        <div
+          tabIndex={0}
+          className="dropdown-content bg-base-100 rounded-box z-1 flex w-50 content-center justify-center p-2 shadow-md"
+        >
+          <div className="flex content-center justify-between">
+            <button className="btn bg- h-[40px] w-[40px] rounded-full"></button>
+            <button className="btn bg- h-[40px] w-[40px] rounded-full"></button>
+            <button className="btn bg- h-[40px] w-[40px] rounded-full"></button>
+            <button className="btn bg- h-[40px] w-[40px] rounded-full"></button>
+          </div>
+        </div>
+      </div>
+      {/*OPTION 2 WITH MENU*/}
+      <ul className="menu menu-md bg-base-200 rounded-box w-full">
+        <li>
+          <details>
+            <summary className="text-base font-semibold">Filter by...</summary>
+            <ul>
+              <li>
+                <details>
+                  <summary>Date</summary>
+                  <ul>
+                    <li>
+                      <label>
+                        <input
+                          type="checkbox"
+                          className="checkbox checkbox-sm"
+                        />
+                        Newest to Oldest
+                      </label>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+              <li>
+                <details>
+                  <summary>Tag</summary>
+                  <ul>
+                    <li>
+                      <label>
+                        <input
+                          type="checkbox"
+                          className="checkbox checkbox-sm"
+                        />
+                        Newest to Oldest
+                      </label>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </details>
+        </li>
+      </ul>
     </div>
   );
 };
