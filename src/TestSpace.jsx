7b729d0f3565538c80a0ae6e17b4ce5acc9d2db0
a.rivera <a.riveraa99@gmail.com>
2025-03-18 23:34:57 -0500

    Added remove function from Note.jsx

--- src/TestSpace.jsx
+++ src/TestSpace.jsx
@@ -1,20 +1,30 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useMemo } from "react";
 import { NoteListContext } from "./components/NoteListContext";
 
 const TestSpace = () => {
   // ? main functions: add note, keep track of inner texts, filter, search, delete
   const { notesList, setNotesList } = useContext(NoteListContext);
   const [color, setColor] = useState("");
-  const [title, setTitle] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  //* SEARCH - run only if anything in list or searchTerm changes
+  const searchList = useMemo(() => {
+    return notesList.filter(
+      (note) =>
+        note.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        note.description.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+  }, [notesList, searchTerm]);
+  // * FILTERS - DATE AND COLOR
+
+  // * ADD NEW NOTE
   // when btn is pressed, empty object is created with special id
   const handleNewNote = () => {
     setNotesList([
       ...notesList,
       { title: "", description: "", color: "", id: crypto.randomUUID() },
     ]);
-    setTitle("");
   };
-  // when title is
+  //* INPUT CHANGE EVENTS
   const handleTitleChange = (id, e) => {
     const userInput = e.target.value;
     setNotesList((prevNotes) =>
@@ -33,20 +43,52 @@ const TestSpace = () => {
     );
   };
 
-  console.log(notesList);
+  // * DELETE NOTE
+  const handleRemove = (id) => {
+    const updatedNotes = notesList.filter((note) => {
+      return note.id !== id;
+    });
+
+    setNotesList(updatedNotes);
+  };
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
-      <button className="btn btn-accent" onClick={handleNewNote}>
-        Add New Note
-      </button>
+      <input
+        className="input"
+        type="search"
+        placeholder="Search notes..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className="join">
+        <button className="btn btn-accent rounded-xl" onClick={handleNewNote}>
+          New
+        </button>
+        <button className="btn btn-success rounded-xl" onClick={handleNewNote}>
+          New
+        </button>
+        <button className="btn btn-primary rounded-xl" onClick={handleNewNote}>
+          New
+        </button>
+        <button className="btn btn-warning rounded-xl" onClick={handleNewNote}>
+          New
+        </button>
+      </div>
+
       {/* simplifying everything with useContext */}
 
-      {notesList.map((note) => (
+      {searchList.map((note) => (
         <div
           key={note.id}
           className="card card-sm h-fit w-fit border border-amber-300"
         >
           <div className="card-body">
+            <button
+              className="btn border-success h-2.5 w-2.5 border"
+              onClick={() => handleRemove(note.id)}
+            >
+              X
+            </button>
             <div className="join join-vertical">
               <input
                 className="input input-warning join-item"
