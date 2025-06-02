117d93b3d521378a4c45f6d0e24e278cdd0772e2
a.rivera <a.riveraa99@gmail.com>
2025-06-02 10:56:22 -0500

    gh-pages merge

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-  resolve: {
-    alias: {
-      crypto: "crypto-browserify",
-    },
-  },
-  base: "/sticky-notes/",
-=======
   base: "/sticky-notes",
->>>>>>> move-states
 });
